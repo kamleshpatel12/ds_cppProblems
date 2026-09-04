f6911b458ae5b2bdf5d945762a460d34002c39bb
Kamlesh Patel <kamleshpatel0217@gmail.com>
2026-09-05 01:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (35.89%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -5,16 +5,31 @@ public:
     if(x<0){
         return false;
     }
-    string s = to_string(x);
-    int i=0;
-    int j = s.size()-1;
-    while(i<j){
-        if(s[i]!=s[j]){
-            return false;
-        }
-        i++;
-        j--;
-     }
-     return true;
+
+    int temp =x;
+   long long result = 0; // while reversing number can overflow, so it is necessary to check for it
+    while(temp!=0){
+        int rem = temp %10;
+        result  = result*10+rem;
+        temp /=10;
+    }
+
+    if(result > INT_MAX || result < INT_MIN) 
+    return false;
+
+    while(x!=0){
+      int rem1 = x%10;
+      int rem2 = result%10;
+      if(rem1!=rem2){
+        return false;
+      }
+      x /=10;
+      result /=10;
+    }
+    return true;
+
     }
 };
+
+
+
